0f4932d8a401d5ad619915f7b87a39e3b590738f
Felipe Dessotti Inoue <felipeinoue@hotmail.com>
2024-10-10 20:19:47 -0300

    before adding react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Main from './components/Main';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Homepage />
     </>
   );
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,17 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+
+
+function Homepage() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default Homepage;
